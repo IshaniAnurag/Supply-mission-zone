45419459cadc7e664316a2214103c6029fd84b5e
IshaniAnurag <67219467+IshaniAnurag@users.noreply.github.com>
2020-07-14 21:15:22 +0530

    Add files via upload

---
+++ redlogs.js
@@ -0,0 +1,24 @@
+class redlogs{
+    constructor(x,y,width,height){
+        var options={
+            
+           
+            isStatic:true
+            
+        }
+        this.body=Bodies.rectangle(x,y,width,height,options);
+        this.width=width;
+        this.height=height;
+        World.add(world,this.body);
+    }
+    display(){
+    var pos=this.body.position;
+    
+   //push();
+  //  translate(this.body.position.x,this.body.position.y);
+    rectMode(CENTER);
+    fill("red");
+    rect(pos.x,pos.y,this.width,this.height);
+   // pop();
+     }
+}
